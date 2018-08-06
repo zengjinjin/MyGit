e6fbb1c4cba1554c86cc0452e272bf9e31e7c848
zengjinjin <347527416@qq.com>
2018-08-06 13:56:18 +0800

    自定义ViewGroup

--- app/src/main/java/com/zjj/cosco/view/MyNinePhotoView.java
+++ app/src/main/java/com/zjj/cosco/view/MyNinePhotoView.java
@@ -23,10 +23,46 @@ public class MyNinePhotoView extends ViewGroup {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        //测量所有子控件的宽和高,只有先测量了所有子控件的尺寸，后面才能使用child.getMeasuredWidth()
-        measureChildren(widthMeasureSpec,heightMeasureSpec);
         //调用系统的onMeasure一般是测量自己(当前ViewGroup)的宽和高
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        //wrap_content
+        int realWidth = 0;
+        int realHeight = 0;
+
+        int lineWidth = 0;
+        int lineHeight = 0;
+
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++){
+            View childView = getChildAt(i);
+            if (childView.getVisibility() == GONE){
+                continue;
+            }
+            measureChild(childView, widthMeasureSpec, heightMeasureSpec);
+            MarginLayoutParams params = (MarginLayoutParams) childView.getLayoutParams();
+            int childWidth = childView.getMeasuredWidth() + params.leftMargin + params.rightMargin;
+            int childHeight = childView.getMeasuredHeight() + params.topMargin + params.bottomMargin;
+            if (lineWidth + childWidth > widthSize - getPaddingLeft() - getPaddingRight()){//需要换行
+                realWidth = Math.max(realWidth, lineWidth);//上一行的宽度
+                realHeight = realHeight + lineHeight;//上一行的高度
+                lineWidth = childWidth;//当前行的宽度
+                lineHeight = childHeight;//当前行的高度
+            }else {
+                lineWidth = lineWidth + childWidth;
+                lineHeight = Math.max(lineHeight, childHeight);
+            }
+            if (i == childCount - 1){
+                realWidth = Math.max(realWidth, lineWidth);
+                realHeight = realHeight + lineHeight;
+            }
+        }
+        setMeasuredDimension(widthMode == MeasureSpec.EXACTLY ? widthSize : realWidth + getPaddingLeft() + getPaddingRight(),
+                heightMode == MeasureSpec.EXACTLY ? heightSize : realHeight + getPaddingTop() + getPaddingBottom());
     }
 
     @Override
@@ -35,22 +71,25 @@ public class MyNinePhotoView extends ViewGroup {
         KLog.i("zjj","mViewGroupWidth="+GuideUtil.px2dp(getContext(),mViewGroupWidth));
         KLog.i("zjj","l="+l);
         KLog.i("zjj","t="+t);
-        int x = l + getPaddingLeft();//当前绘制光标的X坐标
+        int x = getPaddingLeft();//当前绘制光标的X坐标
         int y = getPaddingTop();//当前绘制光标的Y坐标
+        int lineHeight = 0;
         KLog.i("zjj","x="+ GuideUtil.px2dp(getContext(), x)+",y="+GuideUtil.px2dp(getContext(), y));
         int childCount = getChildCount();
         for (int i = 0; i < childCount; i++){
             View childView = getChildAt(i);
             int childWidth = childView.getMeasuredWidth();
             int childHeight = childView.getMeasuredHeight();
-            MyLayoutParams params = (MyLayoutParams) childView.getLayoutParams();
+            MarginLayoutParams params = (MarginLayoutParams) childView.getLayoutParams();
             if (x + childWidth + params.leftMargin + params.rightMargin + getPaddingRight() > mViewGroupWidth){
-                x = l + getPaddingLeft();
-                y = y + childHeight + params.topMargin + params.bottomMargin;
+                x = getPaddingLeft();
+                y = y + lineHeight;
+            }else {
+                lineHeight = Math.max(lineHeight, childHeight + params.topMargin + params.bottomMargin);
             }
             childView.layout(x + params.leftMargin, y + params.topMargin,
                     x + childWidth + params.leftMargin, y + childHeight + params.topMargin);
-            x = x + childWidth + params.rightMargin;//下一次开始绘制的x坐标
+            x = x + params.leftMargin + childWidth + params.rightMargin;//下一次开始绘制的X坐标
         }
     }
 
