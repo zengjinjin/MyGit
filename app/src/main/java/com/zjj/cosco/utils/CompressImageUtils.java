efef9fff3830b32bec0bcdafffe61f845464a425
zengjinjin <347527416@qq.com>
2018-08-14 16:20:46 +0800

    图片压缩

---
+++ app/src/main/java/com/zjj/cosco/utils/CompressImageUtils.java
@@ -0,0 +1,128 @@
+package com.zjj.cosco.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by 路易十三 on 2017/5/15 0015.
+ */
+public class CompressImageUtils {
+
+    /**
+     *
+     * @param filePath 本地路径
+     * @param targetPath 压缩后路径
+     * @param quality 压缩质量
+     * @return
+     */
+    public static File compressImage(String filePath, String targetPath, int quality) {
+        Bitmap bm = getSmallBitmap(filePath);//获取一定尺寸的图片
+        int degree = readPictureDegree(filePath);//获取相片拍摄角度
+        if (degree != 0) {//旋转照片角度，防止头像横着显示
+            bm = rotateBitmap(bm, degree);
+        }
+        File outputFile = new File(targetPath);
+        try {
+            if (!outputFile.exists()) {
+                outputFile.getParentFile().mkdirs();
+                //outputFile.createNewFile();
+            } else {
+                outputFile.delete();
+            }
+            FileOutputStream out = new FileOutputStream(outputFile);
+            bm.compress(Bitmap.CompressFormat.JPEG, quality, out);
+        } catch (Exception e) {
+        }
+        return outputFile;
+    }
+
+    /**
+     * 根据路径获得图片信息并按比例压缩，返回bitmap
+     */
+    private static Bitmap getSmallBitmap(String filePath) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;//只解析图片边沿，获取宽高
+        BitmapFactory.decodeFile(filePath, options);
+        // 计算缩放比
+        options.inSampleSize = calculateInSampleSize(options, 480, 800);
+        // 完整解析图片返回bitmap
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(filePath, options);
+    }
+
+
+    /**
+     * 获取照片角度
+     *
+     * @param path
+     * @return
+     */
+    private static int readPictureDegree(String path) {
+        int degree = 0;
+        try {
+            ExifInterface exifInterface = new ExifInterface(path);
+            int orientation = exifInterface.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    degree = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    degree = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    degree = 270;
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return degree;
+    }
+
+    /**
+     * 旋转照片
+     *
+     * @param bitmap
+     * @param degress
+     * @return
+     */
+    private static Bitmap rotateBitmap(Bitmap bitmap, int degress) {
+        if (bitmap != null) {
+            Matrix m = new Matrix();
+            m.postRotate(degress);
+            bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), m, true);
+            return bitmap;
+        }
+        return bitmap;
+    }
+
+    
+    private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+        if (height > reqHeight || width > reqWidth) {
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+//        int inSampleSize = 2; // 默认像素压缩比例，压缩为原图的1/2
+//        int minLen = Math.min(height, width); // 原图的最小边长
+//        if(minLen > 100) { // 如果原始图像的最小边长大于100dp（此处单位我认为是dp，而非px）
+//            float ratio = (float)minLen / 100.0f; // 计算像素压缩比例
+//            inSampleSize = (int)ratio;
+//        }
+        return inSampleSize;
+    }
+
+
+
+}
